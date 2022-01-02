014ddab114cc341adcf179bd8b18115f9f77595f
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-01-02 19:54:57 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,19 @@
+//Q2. Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
+
+
+
+var A = 10;
+var J = 20;
+var k = 8;
+
+if( A > j && A > k)
+{
+    console.log(" A is greatest");
+}
+else if ( h > s && h > u)
+{
+    console.log(" j is greatest");
+}
+else{
+    console.log(" k is greatest")
+}
